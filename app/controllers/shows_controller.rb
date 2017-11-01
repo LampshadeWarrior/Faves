12b5bb4d36d13faaed9e80adf005f136e320bf0d
Iann Singer <itpsinger@gmail.com>
2017-11-01 02:10:28 +0000

    Added a submission form and hooked it up to the database

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,5 +1,16 @@
 class ShowsController < ApplicationController
-  def new
+  def index
+  end
+
+  def create
     @show = Show.new
+    Show.create(show_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def show_params
+    params.require(:show).permit(:name, :person, :city)
   end
 end
