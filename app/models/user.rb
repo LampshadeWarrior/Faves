cba6134d32873612e130049190e2141b456f9308
Iann Singer <itpsinger@gmail.com>
2017-12-22 19:56:59 +0000

    Remove whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-
   has_many :shows
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   acts_as_voter
 end
